add9c6054c0435cdf01508ede3ae93326a99d797
Jhonata Vinicius Da Silva Araujo <52162636+jhony2488@users.noreply.github.com>
2020-02-14 12:30:00 -0300

    Add files via upload

---
+++ js/calculadora.js
@@ -0,0 +1,36 @@
+// variavel  usada  para evitar que duas expressoes matematicas estejam juntas sem um numero no seu meio exemplo: +- ou /-
+var validation = 0;
+var tela = document.querySelector('.form-control');
+// variavel que recebera o resultado da operação.
+var resultado=0;
+var contagem=0;
+
+function calcular(tipo, valor) {
+    if (tipo === 'açao') {
+        if (valor == 'c') {
+            tela.value = '';
+        }
+        if(validation==1&&contagem>=1){
+        
+        if (valor === '+' || valor === '-' || valor === '*' || valor === '/' || valor === '.') {
+            tela.value += valor;
+            validation=0;
+        } 
+        if(valor==='='){
+            resultado=eval(tela.value);
+            tela.value=resultado;
+            validation=1;
+            
+        }
+        }
+
+    }
+    else if (tipo === 'numero') {
+    if(validation==0){
+        tela.value += valor;
+        validation=1;
+        contagem++;
+        }
+           
+        }
+}
